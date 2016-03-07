8f1f02e381890aa7fe1b2d9a5892184152399b7e
Melissa <mactechy@gmail.com>
2016-03-06 23:05:52 -0500

    updated code sample

--- leaderboard.js
+++ leaderboard.js
@@ -2,9 +2,14 @@ PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
   Template.leaderboard.helpers({
-      'player': function(){
-          return PlayersList.find();
-      }
+    'player': function(){
+      return PlayersList.find();
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      console.log("You clicked a .player element");
+    }
   });
 }
 
