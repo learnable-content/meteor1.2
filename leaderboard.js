0e4dd89e83c0c974fa78b4d30ccaac7137f67872
Melissa <mactechy@gmail.com>
2016-03-06 22:47:08 -0500

    updated code sample

--- leaderboard.js
+++ leaderboard.js
@@ -1,13 +1,9 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
-  Template.leaderboard.helpers({
-      'player': function(){
-          return PlayersList.find();
-      }
-  });
-}
+ 
+ }
 
 if(Meteor.isServer){
-  // server only code
+  
 }
