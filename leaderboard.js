bce1a7974771b945b49771902ec173181ab054b8
Melissa <mactechy@gmail.com>
2016-03-07 00:28:35 -0500

    added code samples

---
+++ leaderboard.js
@@ -0,0 +1,34 @@
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find({}, { sort: {score: -1, name: 1} });
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5} });
+    },
+    'click .decrement': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
+    }
+  });
+}
+
+if(Meteor.isServer){
+  // server only code
+}
