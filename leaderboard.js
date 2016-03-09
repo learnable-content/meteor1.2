9d05c833572002ded26c4806e0e95a1fd9ad6c88
Melissa <mactechy@gmail.com>
2016-03-09 09:42:17 -0500

    added code samples

---
+++ leaderboard.js
@@ -0,0 +1,50 @@
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find({}, { sort: {score: -1, name: 1} });
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    },
+    'selectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne({ _id: selectedPlayer });
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5} });
+    },
+    'click .decrement': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
+    }
+    
+  });
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var playerNameVar = event.target.playerName.value;
+        PlayersList.insert({
+            name: playerNameVar,
+            score: 0
+        });
+        event.target.playerName.value = "";
+    }
+  });
+}
+
+if(Meteor.isServer){
+  // server only code
+}
