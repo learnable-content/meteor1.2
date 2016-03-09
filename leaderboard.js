e72a7fec54453269dbf997676c32bd78704ea6e1
Melissa <mactechy@gmail.com>
2016-03-09 10:23:23 -0500

    updated code sample

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,7 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
+  Meteor.subscribe('thePlayers');
   Template.leaderboard.helpers({
     'player': function(){
         var currentUserId = Meteor.userId();
@@ -54,6 +55,7 @@ if(Meteor.isClient){
 
 if(Meteor.isServer){
   Meteor.publish('thePlayers', function(){
-      return PlayersList.find();
-    });
+      var currentUserId = this.userId;
+      return PlayersList.find({ createdBy: currentUserId });
+  });
 }
